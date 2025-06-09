488aee5bf2b3b935621c10dd14778d5962002306
Vihaan <vihaan4263@gmail.com>
2025-06-09 16:30:17 +0530

    new file:   src/app/components/contact/contact.component.css 	new file:   src/app/components/contact/contact.component.html 	new file:   src/app/components/contact/contact.component.spec.ts

---
+++ src/app/components/milestones/milestones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-milestones',
+  imports: [],
+  templateUrl: './milestones.component.html',
+  styleUrl: './milestones.component.css'
+})
+export class MilestonesComponent {
+
+}
---
+++ src/app/components/overview/overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  imports: [],
+  templateUrl: './overview.component.html',
+  styleUrl: './overview.component.css'
+})
+export class OverviewComponent {
+
+}
